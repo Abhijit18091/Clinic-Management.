8353add1f4877a8da57aba9f6ab329d478887046
Abhijit Sharma <132984035+Abhijit18091@users.noreply.github.com>
2024-10-04 00:59:23 +0530

    Add files via upload
    This repository contains a comprehensive Clinic Management System designed to facilitate efficient patient management, appointment scheduling, and billing processes. The system features a user-friendly interface for both patients and healthcare providers, ensuring seamless communication and access to medical records. Visit the Clinic Management Website: http://127.0.0.1:5500/index.html for more information and to see the system in action.

---
+++ main.js
@@ -0,0 +1,21 @@
+//   navbar function 
+$(document).ready(function(){
+
+    $('.fa-bars').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $(window).on('scroll load',function(){
+        $('.fa-bars').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+
+        if($(Window).scrollTop()  >  30){
+            $('header').addClass('header-active');
+        }else{
+            $('header').removeClass('header-active');
+        }
+    });
+
+    
+});
